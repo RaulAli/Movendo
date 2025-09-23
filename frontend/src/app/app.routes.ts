fbad64cb44745d7587a029c35d68dd59bcb7fd38
RaulAli <raualitor@alu.edu.gva.es>
2025-09-23 21:09:46 +0200

    Creacion de frontend, footer y header

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+// src/app/app.routes.ts
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    // { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+    { path: '**', redirectTo: '' },
+];
