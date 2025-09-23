cbd2d80361f195b12c52715d9937ebe219f1792f
RaulAli <raualitor@alu.edu.gva.es>
2025-09-23 21:28:44 +0200

    Creacion de nivel de carpetas app en backend

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const cors = require('cors');
-const prodsRoutes = require('./routes/prods.routes');
-const errorHandler = require('./middlewares/errorHandler');
+const prodsRoutes = require('./app/routes/prods.routes');
+const errorHandler = require('./app/middlewares/errorHandler');
 // const slug = require('slug'); // para CommonJS
 
 const app = express();
@@ -14,7 +14,7 @@ const MONGO_URI = process.env.MONGO_URI;
 app.use(
   helmet({
     contentSecurityPolicy: false,
-    crossOriginEmbedderPolicy: false 
+    crossOriginEmbedderPolicy: false
   })
 );
 
@@ -24,9 +24,9 @@ app.use(express.json());
 const allowedOrigins = ['http://localhost:3001', 'http://localhost:3000'];
 app.use(
   cors({
-    origin: function(origin, callback){
-      if(!origin) return callback(null, true);
-      if(allowedOrigins.indexOf(origin) === -1){
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
         return callback(new Error('CORS policy: acceso denegado para ' + origin), false);
       }
       return callback(null, true);
