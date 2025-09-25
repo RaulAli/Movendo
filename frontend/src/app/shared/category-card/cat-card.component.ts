32184f631b31fae13b9859ff0751caba98ac6cc2
DanielG77 <danielgirgar@gmail.com>
2025-09-25 19:19:49 +0200

    Creacio de Categories en el Frontent

---
+++ frontend/src/app/shared/category-card/cat-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Categories } from '../../models/category.model';
+
+@Component({
+  selector: 'categories-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cat-card.component.html',
+  styleUrls: ['./cat-card.component.scss']
+})
+export class CardCategories {
+  @Input() categories!: Categories;
+}
