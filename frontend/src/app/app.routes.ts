56fccb2e75f78b80787a50101eed5fd7227b80d7
DanielG77 <danielgirgar@gmail.com>
2025-09-26 10:24:56 +0200

    Actualizacion de Rutas a LayzyLoad y Creacion de Shop FRONTENT

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 // src/app/app.routes.ts
 import { Routes } from '@angular/router';
-import { HomePage } from './pages/home/home.page';
+
+
 
 export const routes: Routes = [
     // { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomePage },
+    { path: 'home', loadComponent: () => import('./pages/home/home.page').then(c => c.HomePage) },
+    { path: 'shop', loadComponent: () => import('./pages/shop/shop.page').then(c => c.ShopPage) },
+    // { path: 'shop', component: ShopPage },
     { path: '**', redirectTo: '' },
 ];
