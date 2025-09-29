d92c410528a10449496fb24ab6c9903997756a42
RaulAli <raualitor@alu.edu.gva.es>
2025-09-29 20:28:25 +0200

    Implementacion simple del carousel al details

---
+++ frontend/src/app/pages/details/details-routing.ts
@@ -0,0 +1,12 @@
+// // src/app/pages/details/details-routing.ts
+// import { Routes } from '@angular/router';
+// import { DetailsResolver } from './details-resolver.service';
+
+// export const DETAILS_ROUTES: Routes = [
+//     {
+//         path: ':slug',
+//         loadComponent: () => import('./details.page').then(c => c.DetailsPage),
+//         resolve: { evento: DetailsResolver }
+//     }
+// ];
+// // 
