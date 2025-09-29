1dfcee26d358c6ba07a6c4fde1e324bdfbaa56db
RaulAli <raualitor@alu.edu.gva.es>
2025-09-29 12:33:01 +0200

    Se ha añadido una primera version del details

--- frontend/src/app/shared/card-evento/card-evento.component.ts
+++ frontend/src/app/shared/card-evento/card-evento.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { Evento } from '../../core/models/evento.model';
 
 @Component({
   selector: 'card-evento',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './card-evento.component.html',
   styleUrls: ['./card-evento.component.scss']
 })
