0c52dd02cd8382bfd00c5f68db10e832048602d4
RaulAli <raualitor@alu.edu.gva.es>
2025-10-02 19:40:26 +0200

    Se ha añadido el archivo api service para las rutas

--- frontend/src/app/core/services/evento.service.ts
+++ frontend/src/app/core/services/evento.service.ts
@@ -1,43 +1,41 @@
-// core/services/evento.service.ts
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 import { Evento } from '../models/evento.model';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventoService {
-  private baseUrl = 'http://localhost:3000/api/movendo/evento';
 
-  constructor(private http: HttpClient) { }
+  constructor(private apiService: ApiService) { }
 
   list(): Observable<Evento[]> {
-    return this.http.get<any>(this.baseUrl).pipe(
+    return this.apiService.get(`/evento`).pipe(
       map(res => res.data ?? res)
     );
   }
 
   get(slug: string): Observable<Evento> {
-    return this.http.get<any>(`${this.baseUrl}/${slug}`).pipe(
+    return this.apiService.get(`/evento/${slug}`).pipe(
       map(res => res.data ?? res)
     );
   }
 
   create(evento: Evento): Observable<Evento> {
-    return this.http.post<any>(this.baseUrl, evento).pipe(
+    return this.apiService.post(`/evento`, evento).pipe(
       map(res => res.data ?? res)
     );
   }
 
   update(slug: string, evento: Evento): Observable<Evento> {
-    return this.http.put<any>(`${this.baseUrl}/${slug}`, evento).pipe(
+    return this.apiService.put(`/evento/${slug}`, evento).pipe(
       map(res => res.data ?? res)
     );
   }
 
   delete(slug: string): Observable<any> {
-    return this.http.delete<any>(`${this.baseUrl}/${slug}`).pipe(
+    return this.apiService.delete(`/evento/${slug}`).pipe(
       map(res => res.data ?? res)
     );
   }
