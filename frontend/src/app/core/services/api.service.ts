a3df4f6765e025c9de4925b1a77fada3ef35fa7d
RaulAli <raualitor@alu.edu.gva.es>
2025-10-02 19:40:26 +0200

    Se ha añadido el archivo api service para las rutas

---
+++ frontend/src/app/core/services/api.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ApiService {
+    constructor(private http: HttpClient) { }
+
+    private formatErrors(error: any) {
+        return throwError(() => error.error);
+    }
+
+    get(path: string, params: HttpParams = new HttpParams(), port: number = 3000): Observable<any> {
+        return this.http.get(`${environment.api_url}:${port}${path}`, { params })
+            .pipe(catchError(this.formatErrors));
+    }
+
+    put(path: string, body: object = {}, port = 3000): Observable<any> {
+        return this.http.put(`${environment.api_url}:${port}${path}`, JSON.stringify(body))
+            .pipe(catchError(this.formatErrors));
+    }
+
+    post(path: string, body: any = {}, port: number = 3000): Observable<any> {
+        return this.http.post(`${environment.api_url}:${port}${path}`, body)
+            .pipe(catchError(this.formatErrors));
+    }
+
+    delete(path: any, port: number = 3000): Observable<any> {
+        return this.http.delete(`${environment.api_url}:${port}${path}`)
+            .pipe(catchError(this.formatErrors));
+    }
+}
--- frontend/src/app/core/services/category.service.ts
+++ frontend/src/app/core/services/category.service.ts
@@ -1,35 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
 import { Category } from '../models/category.model';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
-  private baseUrl = 'http://localhost:3000/api/movendo/category';
 
-  constructor(private http: HttpClient) { }
+  constructor(private apiService: ApiService) { }
 
   list(): Observable<Category[]> {
-    return this.http.get<any>(this.baseUrl).pipe(
+    return this.apiService.get('/category').pipe(
       map(res => res.data ?? res)
     );
   }
 
   get(slug: string): Observable<Category> {
-    return this.http.get<Category>(`${this.baseUrl}/${slug}`);
+    return this.apiService.get(`/category/${slug}`);
   }
 
   create(category: Category): Observable<Category> {
-    return this.http.post<Category>(this.baseUrl, category);
+    return this.apiService.post(`/category/`, category);
   }
 
   update(slug: string, category: Category): Observable<Category> {
-    return this.http.put<Category>(`${this.baseUrl}/${slug}`, category);
+    return this.apiService.put(`/category/${slug}`, category);
   }
 
   delete(slug: string): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/${slug}`);
+    return this.apiService.delete(`/category/${slug}`);
   }
 }
