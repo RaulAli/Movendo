6618531a89511dd55e9d16a383fbfc1c8e06f1e7
RaulAli <raualitor@alu.edu.gva.es>
2025-10-02 11:02:25 +0200

    Implementacion de routing y resolver al details

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
 
     { path: 'shop', loadComponent: () => import('./pages/shop/shop.page').then(c => c.ShopPage) },
 
-    { path: 'details/:slug', loadComponent: () => import('./pages/details/details.page').then(c => c.DetailsPage) },
+    { path: 'details', loadChildren: () => import('./pages/details/details-routing').then(m => m.DETAILS_ROUTES) },
 
     { path: '**', redirectTo: '' },
 ];
