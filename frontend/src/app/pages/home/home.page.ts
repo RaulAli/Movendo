68b64d160ba1696cf75bd12115d81123dd24dab9
DanielG77 <danielgirgar@gmail.com>
2025-10-06 17:11:10 +0200

    [FIX] arreglo filtro frontent Applyfiltros

--- frontend/src/app/pages/home/home.page.ts
+++ frontend/src/app/pages/home/home.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselComponent } from '../../shared/carousel/carousel.component';
-import { ListComponent } from '../../shared/list-evento/list-evento.component';
 import { ListCategoryComponent } from '../../shared/list-category/list-category.component';
 @Component({
   selector: 'home-page',
@@ -9,7 +8,6 @@ import { ListCategoryComponent } from '../../shared/list-category/list-category.
   imports: [
     CommonModule,
     CarouselComponent,
-    ListComponent,
     ListCategoryComponent
   ],
   templateUrl: './home.page.html'
