e82c6f8205aef7dc2f7f6b84941f63db868196b7
DanielG77 <danielgirgar@gmail.com>
2025-10-15 00:32:57 +0200

    [FRONTEND] settings_profile_falta_acabar

--- frontend/src/app/pages/profile/profile.page.ts
+++ frontend/src/app/pages/profile/profile.page.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
 @Component({
     selector: 'profile-page',
     standalone: true,
-    imports: [
-        CommonModule
-    ],
-    templateUrl: './profile.page.html'
+    imports: [CommonModule],
+    templateUrl: './profile.page.html',
+    styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage {
 
+    constructor(private router: Router) { }
+
+    goToSettings(): void {
+        this.router.navigate(['/settings']);
+    }
 }
