0c2254f7691ddfbc3e192a074b93da010ba91f91
RaulAli <raualitor@alu.edu.gva.es>
2025-10-14 19:28:15 +0200

    [FRONTEND] Se ha añadido el populate ademas de algunos cambios de visibilidad en el header

--- frontend/src/app/shared/layout/header/header.component.ts
+++ frontend/src/app/shared/layout/header/header.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { UserService } from '../../../core/services/auth.service';
+import { JwtService } from '../../../core/services/jwt.service';
+import { Subscription } from 'rxjs';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgIf],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent { }
+export class HeaderComponent implements OnInit {
+  logged = false;
+  private subscription!: Subscription;
+
+  constructor(
+    private userService: UserService,
+    private JwtService: JwtService,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.subscription = this.userService.isAuthenticated.subscribe((auth) => {
+      this.logged = auth;
+      console.log(auth);
+    });
+
+  }
+
+  logout() {
+    this.JwtService.destroyToken();
+    this.userService.logout()
+    this.router.navigate(['/login']);
+    window.location.reload();
+  }
+}
