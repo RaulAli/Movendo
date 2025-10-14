0413982447f024ab4e453da0fa0b86e22fafefa2
DanielG77 <danielgirgar@gmail.com>
2025-10-14 20:05:38 +0200

    [FRONTEND] Profile basico

---
+++ frontend/src/app/pages/profile/profile.page.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+    selector: 'profile-page',
+    standalone: true,
+    imports: [
+        CommonModule
+    ],
+    templateUrl: './profile.page.html'
+})
+export class ProfilePage {
+
+}
