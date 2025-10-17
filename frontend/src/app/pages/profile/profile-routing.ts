0ce909de4b8e5cdafab656cd4bd1a2e11e14ca4d
RaulAli <raualitor@alu.edu.gva.es>
2025-10-17 11:18:43 +0200

    [FRONTEND] Implementacion de los followers simple

--- frontend/src/app/pages/profile/profile-routing.ts
+++ frontend/src/app/pages/profile/profile-routing.ts
@@ -2,10 +2,15 @@
 import { Routes } from '@angular/router';
 import { ProfilePage } from './profile.page';
 import { AuthGuard } from '../../core/guards';
+
 export const PROFILE_ROUTES: Routes = [
   {
     path: '',
     component: ProfilePage,
     canActivate: [AuthGuard]
+  },
+  {
+    path: ':username',
+    component: ProfilePage,
   },
 ];
--- frontend/src/app/pages/profile/profile.page.ts
+++ frontend/src/app/pages/profile/profile.page.ts
@@ -1,25 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListProfileComponent } from '../../shared/list-profile/list-profile.component';
+import { ActivatedRoute } from '@angular/router';
+import { ProfileService } from '../../core/services/profile.service';
+import { Profile } from '../../core/models/profile.model';
+import { switchMap } from 'rxjs/operators';
+import { UserService } from '../../core/services/auth.service';
 import { SettingsComponent } from '../../shared/settings/settings.component';
+
 @Component({
     selector: 'app-profile-page',
     standalone: true,
-    imports: [CommonModule, ListProfileComponent, SettingsComponent],
-    template: `
-    <list-profile 
-      *ngIf="currentView === 'profile'" 
-      (settingsClicked)="showSettings()">
-    </list-profile>
-    
-    <settings-component 
-      *ngIf="currentView === 'settings'" 
-      (backToProfile)="showProfile()">
-    </settings-component>
-  `
+    imports: [CommonModule, SettingsComponent],
+    templateUrl: './profile.page.html',
+    styleUrls: ['./profile.page.scss']
 })
-export class ProfilePage {
+export class ProfilePage implements OnInit {
+    profile!: Profile;
+    isUser: boolean = false;
     currentView: 'profile' | 'settings' = 'profile';
+
+    constructor(
+        private route: ActivatedRoute,
+        private profileService: ProfileService,
+        private userService: UserService
+    ) { }
+
+    ngOnInit() {
+        this.route.paramMap.pipe(
+            switchMap(params => {
+                const username = params.get('username');
+                if (username) {
+                    return this.profileService.get(username);
+                } else {
+                    return this.userService.currentUser.pipe(
+                        switchMap(user => {
+                            return this.profileService.get(user.username);
+                        })
+                    );
+                }
+            })
+        ).subscribe(profile => {
+            this.profile = profile;
+            this.userService.currentUser.subscribe(user => {
+                this.isUser = user.username === this.profile.username;
+            });
+        });
+    }
+
+    onToggleFollowing() {
+        if (this.profile.following) {
+            this.profileService.unfollow(this.profile.username).subscribe(profile => this.profile = profile);
+        } else {
+            this.profileService.follow(this.profile.username).subscribe(profile => this.profile = profile);
+        }
+    }
 
     showSettings(): void {
         this.currentView = 'settings';
