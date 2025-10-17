2f44032ffdd1dbf2d7e713b64688d1fd66f66ead
RaulAli <raualitor@alu.edu.gva.es>
2025-10-17 09:59:41 +0200

    [BACKEND] Se ha creado los favoritos en el backend

--- backend/app/routes/evento.routes.js
+++ backend/app/routes/evento.routes.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/evento.controller');
+const verifyJWT = require('../middlewares/verifyJWT')
 
 router.get('/evento', controller.listar);
 
@@ -20,5 +21,9 @@ router.get('/category/:slug', controller.GetEventosByCategory);
 router.get('/cities', controller.getUniqueCities);
 
 router.get('/prices/minmax', controller.getMinMaxPrices);
+
+// Favorites
+
+router.post('/evento/:slug/favorite', verifyJWT, controller.addfavoriteEvento);
 
 module.exports = router;
