93ac9a564fd690376e6ecd52dd5d28ed17d6ffc2
RaulAli <raualitor@alu.edu.gva.es>
2025-10-17 11:18:43 +0200

    [FRONTEND] Implementacion de los followers simple

---
+++ frontend/src/app/core/services/profile.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ApiService } from './api.service';
+import { Profile } from '../models/profile.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProfileService {
+    constructor(private apiService: ApiService) { }
+
+    get(username: string): Observable<Profile> {
+        return this.apiService.get(`/profile/${username}`)
+            .pipe(map(data => data.profile));
+    }
+
+    follow(username: string): Observable<Profile> {
+        return this.apiService.post(`/profile/${username}/follow`)
+            .pipe(map(data => data.profile));
+    }
+
+    unfollow(username: string): Observable<Profile> {
+        return this.apiService.delete(`/profile/${username}/follow`)
+            .pipe(map(data => data.profile));
+    }
+}
