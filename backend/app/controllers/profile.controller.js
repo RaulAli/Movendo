3b73d5b2e89941a57ffb81358ffd9fc4364b000a
RaulAli <raualitor@alu.edu.gva.es>
2025-10-17 12:40:51 +0200

    [FEATURE] Se ha añadido para poder navegar mediante los followers y following profiles

--- backend/app/controllers/profile.controller.js
+++ backend/app/controllers/profile.controller.js
@@ -35,6 +35,8 @@ exports.followUser = async (req, res, next) => {
         }
 
         await loggedinUser.follow(userToFollow._id);
+        loggedinUser.followingCount = loggedinUser.followingUsers.length;
+        await loggedinUser.save();
         const data = await userToFollow.updateFollowersCount(loggedinUser);
 
         return res.status(200).json({ profile: userToFollow.toProfileJSON(loggedinUser) });
@@ -58,9 +60,57 @@ exports.unfollowUser = async (req, res, next) => {
         }
 
         await loggedinUser.unfollow(userToUnfollow._id);
+        loggedinUser.followingCount = loggedinUser.followingUsers.length;
+        await loggedinUser.save();
         await userToUnfollow.updateFollowersCount(loggedinUser);
 
         return res.status(200).json({ profile: userToUnfollow.toProfileJSON(loggedinUser) });
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.getFollowers = async (req, res, next) => {
+    try {
+        const { username } = req.params;
+        const loggedinUserId = req.userId;
+
+        const [user, loggedinUser] = await Promise.all([
+            User.findOne({ username }).exec(),
+            loggedinUserId ? User.findById(loggedinUserId).exec() : null
+        ]);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const followers = await User.find({ followingUsers: user._id }).exec();
+        const profiles = followers.map(follower => follower.toProfileJSON(loggedinUser));
+
+        return res.status(200).json({ profiles });
+    } catch (err) {
+        next(err);
+    }
+};
+
+exports.getFollowing = async (req, res, next) => {
+    try {
+        const { username } = req.params;
+        const loggedinUserId = req.userId;
+
+        const [user, loggedinUser] = await Promise.all([
+            User.findOne({ username }).exec(),
+            loggedinUserId ? User.findById(loggedinUserId).exec() : null
+        ]);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const following = await User.find({ _id: { $in: user.followingUsers } }).exec();
+        const profiles = following.map(_followingUser => _followingUser.toProfileJSON(loggedinUser));
+
+        return res.status(200).json({ profiles });
     } catch (err) {
         next(err);
     }
--- backend/app/routes/profile.routes.js
+++ backend/app/routes/profile.routes.js
@@ -2,11 +2,16 @@ const express = require('express');
 const router = express.Router();
 const verifyJWT = require('../middlewares/verifyJWT');
 const controller = require('../controllers/profile.controller');
+const optionalVerifyJWT = require('../middlewares/optionalVerifyJWT');
 
-router.get('/:username', controller.getProfile);
+router.get('/:username', optionalVerifyJWT, controller.getProfile);
 
 router.post('/:username/follow', verifyJWT, controller.followUser);
 
 router.delete('/:username/follow', verifyJWT, controller.unfollowUser);
+
+router.get('/:username/followers', optionalVerifyJWT, controller.getFollowers);
+
+router.get('/:username/following', optionalVerifyJWT, controller.getFollowing);
 
 module.exports = router;
