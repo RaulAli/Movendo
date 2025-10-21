d947f1a3f4c001b31904c3b390c60cb8f5e365a2
RaulAli <raualitor@alu.edu.gva.es>
2025-10-21 20:24:56 +0200

    [FRONTEND] Inserccion de treURL

--- frontend/src/app/core/guards/auth-guard.service.ts
+++ frontend/src/app/core/guards/auth-guard.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { UserService } from '../services/auth.service';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
   constructor(private userService: UserService, private router: Router) { }
 
-  canActivate(): boolean {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     const user = this.userService.getCurrentUser();
     if (user && (user.token || Object.keys(user).length > 0)) {
       return true;
     }
-    this.router.navigate(['/auth/login']);
-    return false;
+    // Redirect to the login page with the returnUrl
+    return this.router.createUrlTree(['/auth/login'], { queryParams: { returnUrl: state.url } });
   }
 }
