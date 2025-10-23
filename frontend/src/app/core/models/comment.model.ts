c10c7e0889e7c613750736c8aa3ae8ac87dc93dc
RaulAli <raualitor@alu.edu.gva.es>
2025-10-24 01:52:04 +0200

    [FRONTEND] Se ha implementado para que aparezca en el comentario el evento en el que esta relacionado

--- frontend/src/app/core/models/comment.model.ts
+++ frontend/src/app/core/models/comment.model.ts
@@ -1,8 +1,10 @@
 import { Profile } from './profile.model';
+import { Evento } from './evento.model';
 
 export interface Comment {
   id: number;
   body: string;
   createdAt: string;
   author: Profile;
+  evento?: Evento;
 }
