bdb30b5fd530c57e0e28d4cf03d6fbe4de95cb30
RaulAli <raualitor@alu.edu.gva.es>
2025-11-07 16:30:13 +0100

    [BACKEND] Actualizacion y borrado de eventos

--- prisma/src/services/evento.service.ts
+++ prisma/src/services/evento.service.ts
@@ -3,5 +3,18 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const getAllEventos = async () => {
-  return prisma.evento.findMany();
+  return prisma.eventos.findMany();
+};
+
+export const updateEvento = async (slug: string, data: any) => {
+  return prisma.eventos.update({
+    where: { slug },
+    data,
+  });
+};
+
+export const deleteEvento = async (slug: string) => {
+  return prisma.eventos.delete({
+    where: { slug },
+  });
 };
