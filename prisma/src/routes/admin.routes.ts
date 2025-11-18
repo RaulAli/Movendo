c50fbf78e5341176692fc2b4da4eaadad38ebca9
RaulAli <raualitor@alu.edu.gva.es>
2025-11-18 20:23:54 +0100

    [PRISMA] Restricciones para administradores

--- prisma/src/routes/admin.routes.ts
+++ prisma/src/routes/admin.routes.ts
@@ -4,7 +4,7 @@ import { createAdminSchema, adminLoginSchema } from '../schemas/admin.schema';
 import { adminAuthMiddleware } from '../middlewares/adminAuth.middleware';
 
 async function adminRoutes(fastify: FastifyInstance) {
-  fastify.post('/admins', { schema: { ...createAdminSchema, summary: 'Create a new admin (admin)', tags: ['admins'] } }, adminController.createAdmin);
+  fastify.post('/admins', { schema: { ...createAdminSchema, summary: 'Create a new admin (admin)', tags: ['admins'] }, preHandler: [adminAuthMiddleware] }, adminController.createAdmin);
   fastify.post('/admins/login', { schema: { ...adminLoginSchema, summary: 'Login as an admin', tags: ['admins'] } }, adminController.login);
 }
 
